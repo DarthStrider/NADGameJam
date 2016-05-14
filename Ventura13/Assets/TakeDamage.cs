dd06e798d77021f8e3e1fe7d66e9d2a3fd362b57
Pascal Asselin <pascal.asselin@polymtl.ca>
2016-05-14 02:20:09 -0400

    destroying asteroids yo

---
+++ Ventura13/Assets/TakeDamage.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TakeDamage : MonoBehaviour {
+
+	private float health = 50;
+
+	void Start () {
+	
+	}
+
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.transform.tag == "Bullet") {
+			health -= 5;
+			Destroy (other.gameObject);
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (health <= 0) {
+			Destroy (this.gameObject);
+		}
+	}
+}
