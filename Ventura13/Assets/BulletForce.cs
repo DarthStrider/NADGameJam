d641628bb3ea7f2c7c110d3db928a1329db86f7b
Pascal Asselin <pascal.asselin@polymtl.ca>
2016-05-14 01:37:15 -0400

    shooting

---
+++ Ventura13/Assets/BulletForce.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletForce : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	private float impulseSpeed = 50.0f;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		transform.Rotate (0, 0, 90);
+		rb.AddForce (Vector2.up * impulseSpeed, ForceMode2D.Impulse);
+	}
+	
+}
---
+++ Ventura13/Assets/shoot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shoot : MonoBehaviour {
+
+	public Transform bulletSpawn;
+	public GameObject bullet;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.LeftControl)) {
+			Instantiate (bullet, bulletSpawn.position, Quaternion.identity);
+		}
+	}
+}
