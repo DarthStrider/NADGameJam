0eb3668cfc19c6d3e72c9b507adb32b1844288ed
Pascal Asselin <pascal.asselin@polymtl.ca>
2016-05-14 10:04:40 -0400

    StartTractor

---
+++ Ventura13/Assets/StartBeam.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartBeam : MonoBehaviour {
+
+    int counter = 0;
+    int sideCounter = 0;
+    public bool pullIn = false;
+    GameObject objPulled;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        counter++;
+        if(counter>2 && sideCounter < 10)
+        {
+            counter = 0;
+            transform.localScale += new Vector3(0.1f, 0.5f, 0);
+            sideCounter++;
+        }
+
+
+	}
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        
+        if (collider.gameObject.tag == "Parts")
+        {
+            Debug.Log("InHERE1");
+            pullIn = true;
+            objPulled = collider.gameObject;
+
+        }
+    }
+
+    public GameObject pulling()
+    {
+        return objPulled;
+    }
+
+}
---
+++ Ventura13/Assets/tractorBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tractorBehaviour : MonoBehaviour {
+
+    public GameObject beam;
+    GameObject newBeam;
+    bool unlock = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            newBeam = Instantiate(beam, transform.position, Quaternion.identity) as GameObject;
+            unlock = true;
+        }
+	    if(unlock == true)
+        {
+            if (newBeam.GetComponent<StartBeam>().pullIn == true)
+            {
+                Debug.Log("OKAY");
+                Vector3 beamPos = newBeam.GetComponent<StartBeam>().pulling().transform.position;
+                beamPos = Vector2.MoveTowards(new Vector2(beamPos.x, beamPos.y), transform.position, 100 * Time.deltaTime);
+            }
+        }
+	}
+}
