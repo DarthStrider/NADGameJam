761a46c17db0e84a580bf037de5236ce2c89647b
Pascal Asselin <pascal.asselin@polymtl.ca>
2016-05-14 08:31:50 -0400

    head bob

---
+++ Ventura13/Assets/HeadBob.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadBob : MonoBehaviour {
+
+	private Animator anim;
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+
+	}
+	
+	public void increaseSpeed(int speed){
+		anim.speed = speed;
+	}
+}
--- Ventura13/Assets/PlayerMovement.cs
+++ Ventura13/Assets/PlayerMovement.cs
@@ -4,12 +4,16 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 	public int playerNumber;
 
-	private Rigidbody2D rb;
-	private BoxCollider2D bc;
+
 	public GameObject theArm;
 	public GameObject theHead;
 
+
+	private Rigidbody2D rb;
+	private BoxCollider2D bc;
+	private Animator anim;
 	private RotateArm arm;
+	private HeadBob bobSpeed;
 	private float overcast = 0.1f;
 	private float moveSpeed = 4.0f;
 	private float jumpSpeed = 750.0f;
@@ -20,6 +24,8 @@ public class PlayerMovement : MonoBehaviour {
 		rb = GetComponent<Rigidbody2D> ();
 		bc = GetComponent<BoxCollider2D> ();
 		arm = theArm.GetComponent<RotateArm> ();
+		bobSpeed = theHead.GetComponent<HeadBob> ();
+
 	}
 
 	void OnCollisionEnter2D(Collision2D other)
@@ -41,16 +47,20 @@ public class PlayerMovement : MonoBehaviour {
 		{
 			arm.RotateTheArmLeft ();
 			theHead.transform.eulerAngles = new Vector3(theHead.transform.eulerAngles.x, 180.0f, theHead.transform.eulerAngles.z);
+			bobSpeed.increaseSpeed (2);
+
 		}
 		else if	(Input.GetAxis ("LeftAnalogHorizontal" + playerNumber) < 0)
 		{
 			arm.RotateTheArmRight ();
 			theHead.transform.eulerAngles = new Vector3(theHead.transform.eulerAngles.x, 0.0f, theHead.transform.eulerAngles.z);
+			bobSpeed.increaseSpeed (2);
 		}
 
 		if (Input.GetAxis ("LeftAnalogHorizontal" + playerNumber) == 0)
 		{
 			arm.ResetArm ();
+			bobSpeed.increaseSpeed (1);
 		}
 
 		string[] joys = Input.GetJoystickNames (); 
