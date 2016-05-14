e8766392bac5dfe1fe1c760b584855c8b6524095
SpookyDo <sauce@sauce.com>
2016-05-13 22:07:07 -0400

    Matt`s Update

---
+++ Ventura13/Assets/ShipMovment.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMovment : MonoBehaviour {
+
+	Rigidbody2D rb;
+	private float verticalMoveSpeed = 5.0f;
+	private float horizontalMoveSpeed = 10.0f;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+
+	public void MoveShipHorizontal(float speed){
+		rb.velocity += Vector2.right * speed * horizontalMoveSpeed;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		rb.velocity = Vector2.up * verticalMoveSpeed;
+
+		if(Input.GetAxis("Horizontal") < 0 || Input.GetAxis("Horizontal") > 0){
+			MoveShipHorizontal (Input.GetAxis ("Horizontal"));
+		}
+	}
+}
