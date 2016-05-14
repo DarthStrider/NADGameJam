65493f88d18da7c65f44557627ef46aff7245330
Pascal Asselin <pascal.asselin@polymtl.ca>
2016-05-14 03:52:11 -0400

    camera scipt

---
+++ Ventura13/Assets/CameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public GameObject ship;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(0, ship.transform.position.y + 9, -1);
+	}
+}
