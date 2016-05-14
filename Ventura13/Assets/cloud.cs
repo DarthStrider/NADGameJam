6efa9212e80c0c1dd32d58fe86e2a026267416b0
Pascal Asselin <pascal.asselin@polymtl.ca>
2016-05-14 04:04:37 -0400

    cloud generation

---
+++ Ventura13/Assets/cloud.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cloud : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D (Collision2D col)
+    {
+        Debug.Log("Hit");
+        if (col.gameObject.tag == "Cloud")
+        {
+            Destroy(this);
+        }
+    }
+}
