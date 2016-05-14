2ae2da2eebdccd68d70ba1f1e8a2e20af60187aa
Pascal Asselin <pascal.asselin@polymtl.ca>
2016-05-14 00:11:33 -0400

    Gun_Push

---
+++ Ventura13/Assets/Gun/GunBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunBehaviour : MonoBehaviour {
+    float joyX;
+    float joyY;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void GunMovement(Input direction) {
+        joyX = Input.GetAxis("Horizontal");
+        joyY = Input.GetAxis("Vertical");
+    }
+
+}
