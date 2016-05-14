181ddc569f11ff8ba29a91aa86133effc89d0d55
Pascal Asselin <pascal.asselin@polymtl.ca>
2016-05-14 05:13:50 -0400

    gun rotation

--- Ventura13/Assets/Gun/GunBehaviour.cs
+++ Ventura13/Assets/Gun/GunBehaviour.cs
@@ -1,21 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GunBehaviour : MonoBehaviour {
-    float joyX;
-    float joyY;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-    public void GunMovement(Input direction) {
-        joyX = Input.GetAxis("Horizontal");
-        joyY = Input.GetAxis("Vertical");
+public class GunBehaviour : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GunMovement(Vector2.zero);
+    }
+    public void GunMovement(Vector2 input)
+    {
+        //Debug.Log(Input.GetJoystickNames().Length);
+        float joyX = Input.GetAxis("RightAnalogHorizontal1");
+        float joyY = Input.GetAxis("RightAnalogVertical1");
+        if (joyX != 0 || joyY != 0)
+        {
+            //Debug.Log("JoyX: " + joyX);
+            //Debug.Log("JoyY: " + joyY);
+
+            float angle             = Mathf.Atan2(joyX, joyY) * Mathf.Rad2Deg;
+            float adjustedAngle     = (angle < 0.0f) ? Mathf.Abs(angle) : (360.0f - angle);
+            adjustedAngle           = Mathf.Clamp(adjustedAngle, 90.0f, 270.0f);
+            adjustedAngle           = Mathf.Lerp(transform.localRotation.eulerAngles.z, adjustedAngle, rotationSpeed * Time.deltaTime);
+            transform.localRotation = Quaternion.Euler(new Vector3(0, 0, adjustedAngle));
+        }
     }
 
 }
