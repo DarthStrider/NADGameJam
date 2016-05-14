fecef2d003b62daf2396ead73fec258b5d877202
Pascal Asselin <pascal.asselin@polymtl.ca>
2016-05-14 03:43:30 -0400

    no message

--- Ventura13/Assets/TakeDamage.cs
+++ Ventura13/Assets/TakeDamage.cs
@@ -10,7 +10,7 @@ public class TakeDamage : MonoBehaviour {
 	}
 
 
-	void OnCollisionEnter2D(Collision2D other)
+	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.transform.tag == "Bullet") {
 			health -= 5;
