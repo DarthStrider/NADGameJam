187a2d738899383cf679e9b02394643f8a5a4ff5
Pascal Asselin <pascal.asselin@polymtl.ca>
2016-05-14 08:59:52 -0400

    no message

---
+++ Ventura13/Assets/SwapSprites.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwapSprites : MonoBehaviour {
+
+	public GameObject sprite1;
+	public GameObject sprite2;
+
+	public void SwapFace(){
+		sprite1.GetComponent<SpriteRenderer> ().enabled = false;
+		sprite2.GetComponent<SpriteRenderer> ().enabled = true;
+	}
+
+	public void SwapSide(){
+		sprite1.GetComponent<SpriteRenderer> ().enabled = true;
+		sprite2.GetComponent<SpriteRenderer> ().enabled = false;
+	}
+}
