0d3814f14f5a2567ea9ee10136bd53b1f63351a5
Pascal Asselin <pascal.asselin@polymtl.ca>
2016-05-14 04:08:06 -0400

    destroy bullets

--- Ventura13/Assets/BulletForce.cs
+++ Ventura13/Assets/BulletForce.cs
@@ -6,15 +6,19 @@ public class BulletForce : MonoBehaviour {
 	private Rigidbody2D rb;
 	private float impulseSpeed = 50.0f;
 	private Vector3 bulletWorldPoint;
+	private Renderer render;
 
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
 		transform.Rotate (0, 0, 90);
 		rb.AddForce (Vector2.up * impulseSpeed, ForceMode2D.Impulse);
+		render = GetComponent<Renderer> ();
 	}
 
 
-	void OnBecameInvisible(){
-		enabled = false;
+	void Update(){
+		if (render.isVisible == false) {
+			Destroy (this.gameObject);
+		}
 	}
 }
