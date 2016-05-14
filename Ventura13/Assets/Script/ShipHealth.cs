834653f26d9e9a9e471af7892e7e092e7e0fbcb5
Pascal Asselin <pascal.asselin@polymtl.ca>
2016-05-14 08:30:29 -0400

    set up a ship health script

---
+++ Ventura13/Assets/Script/ShipHealth.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipHealth : MonoBehaviour
+{
+    public float health;
+    private float maxHealth;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        maxHealth = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health<=50 && health > 40)
+        {
+
+        }
+        if(health<=40 && health > 30)
+        {
+
+        }
+        if(health <=30 && health > 20)
+        {
+
+        }
+    }
+
+    public void TakeDamage(float x)
+    {
+        if (health - x <= 0)
+        {
+
+        }
+        else
+        {
+            health -= x;
+        }
+    }
+
+    public void healDamage(float x)
+    {
+        if (health + x > maxHealth)
+        {
+            health = maxHealth;
+        }
+        else
+        {
+            maxHealth += x;
+        }
+    }
+}
