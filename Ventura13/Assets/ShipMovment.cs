451805220b04c4ef2c6a8e1f3ec6503fcfd209fa
Pascal Asselin <pascal.asselin@polymtl.ca>
2016-05-14 10:48:07 -0400

    astroids and horizontal movement

--- Ventura13/Assets/ShipMovment.cs
+++ Ventura13/Assets/ShipMovment.cs
@@ -3,26 +3,38 @@ using System.Collections;
 
 public class ShipMovment : MonoBehaviour {
 
-	Rigidbody2D rb;
-	private float verticalMoveSpeed = 3.0f;
-	private float horizontalMoveSpeed = 10.0f;
+	private Rigidbody2D rb;
+	private Vector3 xMin;
+	private Vector3 xMax;
+	public BoxCollider2D box;
+	private float horizontalMoveSpeed = 2.0f;
 
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
+		xMin = Camera.main.ScreenToWorldPoint (new Vector2(0,0));
+		xMax = Camera.main.ScreenToWorldPoint (new Vector2(Screen.width, 0));
+
 	}
 
 
 	public void MoveShipHorizontal(float speed){
-		rb.velocity += Vector2.right * speed * horizontalMoveSpeed;
+		rb.velocity = new Vector2 (speed * horizontalMoveSpeed, rb.velocity.y);
 	}
 
 
 	// Update is called once per frame
 	void Update () {
-		rb.velocity = Vector2.up * verticalMoveSpeed;
-
-		if(Input.GetAxis("LeftAnalogHorizontal1") < 0 || Input.GetAxis("LeftAnalogHorizontal1") > 0){
+		
+		Debug.Log (xMax.x);
+			
+		if (Input.GetAxis ("LeftAnalogHorizontal1") < 0 || Input.GetAxis ("LeftAnalogHorizontal1") > 0) {
 			MoveShipHorizontal (Input.GetAxis ("LeftAnalogHorizontal1"));
+		} else {
+			rb.velocity = new Vector2 (0, rb.velocity.y);
 		}
+		Vector3 extent = box.bounds.extents;
+		float x = transform.position.x;
+		x = Mathf.Clamp (transform.position.x, xMin.x+extent.x, xMax.x-extent.x);
+		transform.position = new Vector2 (x, transform.position.y);
 	}
 }
