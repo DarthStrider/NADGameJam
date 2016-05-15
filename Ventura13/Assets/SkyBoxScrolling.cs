2e26d57b12acfb7df746f500cdfa3af33a88fe96
Pascal Dagenais <pascal.dagenais@hotmail.com>
2016-05-14 23:43:57 -0400

    few things done

---
+++ Ventura13/Assets/SkyBoxScrolling.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyBoxScrolling : MonoBehaviour {
+    public bool start;
+    float speed;
+    public float maxSpeed;
+
+	// Use this for initialization
+	void Start () {
+        speed = maxSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (start == true)
+        {
+            transform.position += -transform.up * speed;
+        }
+	}
+}
