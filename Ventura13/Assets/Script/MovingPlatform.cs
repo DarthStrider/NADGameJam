dde0708a40f660d1763295e0e379499515076e0a
Pascal Dagenais <pascal.dagenais@hotmail.com>
2016-05-15 10:22:26 -0400

    moving platform work

---
+++ Ventura13/Assets/Script/MovingPlatform.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MovingPlatform : MonoBehaviour
+{
+	List<GameObject> players = new List<GameObject>();
+	Rigidbody2D rigidbody;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rigidbody = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		foreach (GameObject player in players)
+		{
+			player.GetComponent<Rigidbody2D> ().velocity += rigidbody.velocity;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		Debug.Log ("collision enter!");
+		if (collision.gameObject.tag == "Player")
+		{
+			players.Add(collision.gameObject);
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D collision)
+	{
+		Debug.Log ("collision exit!");
+		if (collision.gameObject.tag == "Player")
+		{
+			players.Remove(collision.gameObject);
+		}
+	}
+}
