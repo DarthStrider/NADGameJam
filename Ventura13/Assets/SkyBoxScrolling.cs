e89eb8dd9e07455baa8aec06a24f82018ec5e19d
Pascal Dagenais <pascal.dagenais@hotmail.com>
2016-05-15 08:49:12 -0400

    player 2 is green

--- Ventura13/Assets/SkyBoxScrolling.cs
+++ Ventura13/Assets/SkyBoxScrolling.cs
@@ -3,14 +3,10 @@ using System.Collections;
 
 public class SkyBoxScrolling : MonoBehaviour {
     public bool start;
-    float speed;
-    public float maxSpeed;
-  
+
 
 	// Use this for initialization
-	void Start () {
-        speed = maxSpeed;
-	}
+	
 	
 	// Update is called once per frame
 	void Update () {
