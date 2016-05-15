c245882cb933214931170ea5ffb0d3fd6fab016d
DavidTran <david.tran@polymtl.ca>
2016-05-14 21:43:28 -0400

    push_button

---
+++ Ventura13/Assets/ButtonPressed.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonPressed : MonoBehaviour {
+    GameObject tempPlayer;
+    public GameObject xButton;
+    GameObject tempXButton;
+    bool unlockPush = false;
+    int counter = 0;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (unlockPush == true)
+        {
+            if (Input.GetButton("X" + tempPlayer.GetComponent<PlayerMovement>().getPlayerNumber()))
+            {
+                tempPlayer.GetComponent<PlayerMovement>().theArm.transform.localEulerAngles = new Vector3(0, 0, -60);
+                Destroy(tempXButton);
+            }
+        }
+
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            tempXButton = Instantiate(xButton, new Vector3(transform.position.x, (transform.position.y + 1f), transform.position.z), Quaternion.identity) as GameObject;
+            tempXButton.transform.parent = GameObject.FindGameObjectWithTag("Ship").transform;
+            //tempPlayer.GetComponent<PlayerMovement>().theArm.GetComponent<RotateArm>().RotateTheArmRight();
+            tempPlayer = collider.gameObject;
+            unlockPush = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject == tempPlayer)
+        {
+            Destroy(tempXButton);
+            tempPlayer = null;
+        }
+    }
+}
