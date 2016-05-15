546a90b4053d2108d6962f9c102e45d9d4a70941
Pascal Dagenais <pascal.dagenais@hotmail.com>
2016-05-15 12:19:10 -0400

    Asteroids_Done

--- Ventura13/Assets/Spawner.cs
+++ Ventura13/Assets/Spawner.cs
@@ -36,7 +36,7 @@ public class Spawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (unlockAsteroids == false)
+        if (unlockAsteroids == true)
         {
             if (timer <= 0)
             {
